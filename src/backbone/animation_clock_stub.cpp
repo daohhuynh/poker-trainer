57d7633227186ef19e242bfec2b094860a2d7a1c
Dao Huynh <ryanhuynh688@gmail.com>
2026-05-14 20:22:41 -0700

    add animation_clock stub implementation

---
+++ src/backbone/animation_clock_stub.cpp
@@ -0,0 +1,29 @@
+#include "backbone/animation_clock.hpp"
+
+namespace poker_trainer::backbone {
+
+std::uint64_t wall_clock_ms() noexcept {
+    return 0;
+}
+
+std::uint64_t animation_time_ms() noexcept {
+    return 0;
+}
+
+bool is_animation_paused() noexcept {
+    return false;
+}
+
+void pause() noexcept {
+}
+
+void resume() noexcept {
+}
+
+void tick(std::uint64_t /*delta_ms*/) noexcept {
+}
+
+void reset_for_testing() noexcept {
+}
+
+}  // namespace poker_trainer::backbone
